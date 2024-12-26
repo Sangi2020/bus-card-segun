fcbf7ce5c50bcf93fe98f427e9eeb23acdcde37f
ali-tltech <murthasaali.tltechnologies@gmail.com>
2024-12-26 16:07:33 +0530

    updated card

--- src/components/demo-meteors.jsx
+++ src/components/demo-meteors.jsx
@@ -4,7 +4,7 @@ import BusinessCard from "./Vcard.jsx";
 export function MeteorDemo() {
   return (
     <div 
-         className=" max-w-[500px] bg-cover bg-center  h-fit rounded-3xl shadow-lg overflow-hidden bg-white relative">
+         className=" max-w-[800px] bg-cover bg-center  h-fit rounded-3xl shadow-lg overflow-hidden bg-white relative">
         {/* <div className="absolute inset-0 rounded-3xl backdrop-blur-xl bg-black/25 z-10"></div> */}
       
       <Meteors number={0} />
